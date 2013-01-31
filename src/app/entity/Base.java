a0ce400fbdd93ab3baaea0c2308b208e1c3240d8
ThinkPad <admin@love320.com>
2013-01-31 18:08:18 +0800

    简单架构完成
    Signed-off-by: ThinkPad <admin@love320.com>

---
+++ src/app/entity/Base.java
@@ -0,0 +1,26 @@
+package app.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass //JPA 基类的标识
+public class Base {
+	
+	private Long id;
+
+	@Id 
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+
+	
+	
+}
--- src/app/entity/User.java
+++ src/app/entity/User.java
@@ -4,20 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-public class User {
-	
-	@Id
-	private Integer id;
+public class User extends Base {
 	
 	private String name;
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
 
 	public String getName() {
 		return name;
