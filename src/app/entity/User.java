df4cffc205974bd397c47c2393a52aaec9a25066
ThinkPad <admin@love320.com>
2013-01-31 17:29:26 +0800

    完成hiberante4
    Signed-off-by: ThinkPad <admin@love320.com>

---
+++ src/app/entity/User.java
@@ -0,0 +1,33 @@
+package app.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	private Integer id;
+	
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+}
