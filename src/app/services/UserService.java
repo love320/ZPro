24926bee00bdf159533288dacd960492d8669994
zhangdi <admin@love320.com>
2013-02-02 14:17:20 +0800

    调整了spring <beans xmlns=
    Signed-off-by: zhangdi <admin@love320.com>

--- src/app/services/UserService.java
+++ src/app/services/UserService.java
@@ -6,5 +6,8 @@ import com.love320.zpro.services.BaseService;
 
 @Service
 public class UserService extends BaseService {
+	
+	public void sd(){
+	}
 
 }
--- src/com/love320/zpro/services/BaseService.java
+++ src/com/love320/zpro/services/BaseService.java
@@ -1,14 +1,21 @@
 package com.love320.zpro.services;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 
 public class BaseService {
-	
+
 	@Autowired
-	protected  JdbcTemplate jdbc;
-	
-	
-	
-	
+	protected JdbcTemplate jdbc;
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
+
 }
